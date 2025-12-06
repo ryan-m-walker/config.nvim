33c02d1bb9c82d020ced2d057e1a9403b850af12
ryan-m-walker <ryan.matthew.walker@gmail.com>
2025-12-06 00:19:29 -0800

    tsgo changes

--- lua/config/style.lua
+++ lua/config/style.lua
@@ -1,5 +1,15 @@
 vim.diagnostic.config {
     float = {
-        border = 'rounded'
-    }
+        border = 'rounded',
+    },
+    underline = true,
+    signs = true,
 }
+
+-- Configure diagnostic highlights to use undercurl
+vim.cmd([[
+  highlight DiagnosticUnderlineError guisp=#db4b4b gui=undercurl
+  highlight DiagnosticUnderlineWarn guisp=#e0af68 gui=undercurl
+  highlight DiagnosticUnderlineInfo guisp=#0db9d7 gui=undercurl
+  highlight DiagnosticUnderlineHint guisp=#1abc9c gui=undercurl
+]])
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -29,24 +29,24 @@ return {
             -- lua
             lspconfig.lua_ls.setup {}
 
-            -- typescript
-            lspconfig.vtsls.setup {
-                settings = {
-                    vtsls = {
-                        experimental = {
-                            maxInlayHintLength = 50,
-                        },
-                        tsserver = {
-                            maxTsServerMemory = 8192, -- 8GB of memory
-                        },
-                    },
-                    typescript = {
-                        tsserver = {
-                            maxTsServerMemory = 8192, -- 8GB of memory
-                        },
-                    },
-                },
-            }
+            -- typescript -- using tsgo
+            -- lspconfig.vtsls.setup {
+            --     settings = {
+            --         vtsls = {
+            --             experimental = {
+            --                 maxInlayHintLength = 50,
+            --             },
+            --             tsserver = {
+            --                 maxTsServerMemory = 8192, -- 8GB of memory
+            --             },
+            --         },
+            --         typescript = {
+            --             tsserver = {
+            --                 maxTsServerMemory = 8192, -- 8GB of memory
+            --             },
+            --         },
+            --     },
+            -- }
 
             -- tailwind
             lspconfig.tailwindcss.setup {}
@@ -107,6 +107,53 @@ return {
                 settings = {
                     clangd = {
                         fallbackFlags = { '-std=c17' },
+                    },
+                },
+            }
+
+            local configs = require "lspconfig.configs"
+            if not configs.tsgo then
+                configs.tsgo = {
+                    default_config = {
+                        cmd = { "tsgo", "--lsp", "--stdio" },
+                        filetypes = {
+                            "javascript",
+                            "javascriptreact",
+                            "javascript.jsx",
+                            "typescript",
+                            "typescriptreact",
+                            "typescript.tsx",
+                        },
+                        root_dir = lspconfig.util.root_pattern(
+                            "tsconfig.json",
+                            "jsconfig.json",
+                            "package.json",
+                            ".git",
+                            "tsconfig.base.json"
+                        ),
+                        settings = {},
+                    },
+                }
+            end
+
+            -- Optimized tsgo setup with performance settings
+            lspconfig.tsgo.setup {
+                flags = {
+                    debounce_text_changes = 150, -- Reduce LSP requests
+                    allow_incremental_sync = true, -- Use incremental sync
+                },
+                on_attach = function(client, bufnr)
+                    -- Disable semantic tokens to reduce load
+                    client.server_capabilities.semanticTokensProvider = nil
+
+                    -- Optionally disable some features that cause freezing
+                    client.server_capabilities.documentFormattingProvider = false
+                    client.server_capabilities.documentRangeFormattingProvider = false
+                end,
+                settings = {
+                    -- Add any tsgo-specific performance settings here
+                    completions = {
+                        completeFunctionCalls = false, -- Reduce completion overhead
                     },
                 },
             }
